408790fa59133927962ced2b6b73c4cf7e2b2d68
tripleo <50311859+tripleo1@users.noreply.github.com>
2025-11-18 05:34:02 -0500

    Splitting
    Change-Id: Ieac7c31345cb582b41d15f39671032327d9d2426

---
+++ elijah-congenial-upper/src/main/java/tripleo/elijah/work/WorkListImpl2.java
@@ -0,0 +1,52 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*
+ * ElijahLang compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
+ */
+package tripleo.elijah.work;
+
+import com.google.common.collect.ImmutableList;
+import com.google.errorprone.annotations.Immutable;
+import org.jetbrains.annotations.NotNull;
+import org.pcollections.TreePVector;
+
+/**
+ * Created 4/26/21 4:24 AM
+ */
+public class WorkListImpl2 implements WorkList {
+    private org.pcollections.PVector<WorkJob> jobs = TreePVector.empty();
+    private boolean isDone;
+
+    @Override
+    public void addJob(final WorkJob aJob) {
+        jobs = jobs.plus(aJob);
+    }
+
+    @Override
+    public @NotNull ImmutableList<WorkJob> getJobs() {
+        return ImmutableList.copyOf(jobs);
+    }
+
+    @Override
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return jobs.isEmpty();
+    }
+
+    @Override
+    public void setDone() {
+        this.isDone = true;
+    }
+}
+
+//
+// vim:set shiftwidth=4 softtabstop=0 noexpandtab:
+//
