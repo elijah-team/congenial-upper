f1c1256dfb53f4e0c8723a808150411e2006448a
tripleo <tripleo@tripleo.milkyway>
2025-11-22 02:57:20 +0000

    fix typo

--- elijah-congenial-upper/src/test/java/tripleo/vendor/batoull22/ExpertSystemTest.java
+++ elijah-congenial-upper/src/test/java/tripleo/vendor/batoull22/ExpertSystemTest.java
@@ -1,19 +1,21 @@
 package tripleo.vendor.batoull22;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 import tripleo.elijah.util.Mode;
 import tripleo.elijah.util.Operation2;
+
+import java.io.InputStream;
 
 import static org.junit.Assert.assertNotSame;
 
 public class ExpertSystemTest {
 
-	//@Ignore
 	@Test
-	public void testOpenfile() {
+	public void testOne() {
 		final EK_ExpertSystem i = new EK_ExpertSystem();
 
-		final Operation2<EK_Reader> ovo2 = i.openfile_2();
+		final Operation2<EK_Reader> ovo2 = openfile_2(i);
 		assertNotSame(ovo2.mode(), Mode.FAILURE);
 
 
@@ -36,4 +38,19 @@ public class ExpertSystemTest {
 		System.out.println("Result of Backwardchaining: " + b);
 		System.out.println(" ");
 	}
+
+	public @NotNull Operation2<EK_Reader> openfile_2(EK_ExpertSystem aSystem) {
+		try {
+			final InputStream stream = getClass().getResourceAsStream("KB3.txt");
+			assert stream != null;
+			return Operation2.success(new EK_Reader1(aSystem, stream));
+		} catch (Exception ex) {
+			System.out.println("Error:the input file dose not exist");
+			return Operation2.failure(ex);
+		}
+	}
 }
+
+//
+//
+//
